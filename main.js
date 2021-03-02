87d408e3665db4cbef9f5d0d6ace3f6c20efc7f6
ashmitkx <ashmitk0507@gmail.com>
2021-03-02 13:48:35 +0530

    Wrapped parsers with functions. Added run button.

--- main.js
+++ main.js
@@ -45,72 +45,86 @@ const comparePrecedence = (token, opStackTop) => {
 	return Math.sign(comparison);
 };
 
-/* Use a regex to captures all numbers, operators and parenthesis from the input into the infix array.*/
-const input = document.getElementById("exp").value;
-const infix = input.match(
-	new RegExp(
-		`${NUM_REGEX.source}|${OP_REGEX.source}|${PAR_REGEX.source}`,
-		"g"
-	)
-);
+/* Return an array of all numbers, operators and parenthesis extracted from the input using a regex.*/
+const parseInput = (input) =>
+	input.match(
+		new RegExp(
+			`${NUM_REGEX.source}|${OP_REGEX.source}|${PAR_REGEX.source}`,
+			"g"
+		)
+	);
 
-// Stack format: [0] (bottom) -----> [length - 1] (top)
-let outStack = [],
-	opStack = [];
+const parseInfix = (infix) => {
+	// Stack format: [0] (bottom) -----> [length - 1] (top)
+	let outStack = [],
+		opStack = [];
 
-let i = 0;
-while (i < infix.length) {
-	let token = infix[i];
-	i++;
+	let i = 0;
+	while (i < infix.length) {
+		let token = infix[i];
+		i++;
 
-	if (tokenIsDigit(token)) {
-		/* Digits are directly pushed into outStack. */
-		outStack.push(token);
-	} else if (token === "(") {
-		/* ")" is directly pushed into opStack. */
-		opStack.push(token);
-	} else if (token === ")") {
-		/* 
-			")" is not added to opStack.
-			Pop all operators from opStack into outStack, until a "(" is found.
-			"(" is to be discarded.
-		*/
-		let opStackTop = opStack.pop();
-		while (opStackTop !== "(") {
-			outStack.push(opStackTop);
-			opStackTop = opStack.pop();
+		if (tokenIsDigit(token)) {
+			/* Digits are directly pushed into outStack. */
+			outStack.push(token);
+		} else if (token === "(") {
+			/* ")" is directly pushed into opStack. */
+			opStack.push(token);
+		} else if (token === ")") {
+			/* 
+				")" is not added to opStack.
+				Pop all operators from opStack into outStack, until a "(" is found.
+				"(" is to be discarded.
+			*/
+			let opStackTop = opStack.pop();
+			while (opStackTop !== "(") {
+				outStack.push(opStackTop);
+				opStackTop = opStack.pop();
+			}
+		} else if (tokenIsOp(token)) {
+			/* Compare the precedence of the operator token (token) and opStackTop */
+			let opStackTop = opStack[opStack.length - 1],
+				precComparison = comparePrecedence(token, opStackTop);
+
+			/* 
+				Continously pop opStackTop into outStack only while:
+					1. opStack has elements.
+					2. opStackTop is not "(".
+					3. opStackTop has higher precedence compared to token OR opStackTop and token have equal precedence, and token is left associative.
+			*/
+			while (
+				opStack.length > 0 &&
+				opStackTop !== "(" &&
+				(precComparison === -1 ||
+					(precComparison === 0 &&
+						ASSOCIATION[token] === LEFT_ASSOCIATION))
+			) {
+				outStack.push(opStack.pop());
+
+				opStackTop = opStack[opStack.length - 1];
+				precComparison = comparePrecedence(token, opStackTop);
+			}
+
+			/* Finally push the token into opStack */
+			opStack.push(token);
 		}
-	} else if (tokenIsOp(token)) {
-		/* Compare the precedence of the operator token (token) and opStackTop */
-		let opStackTop = opStack[opStack.length - 1],
-			precComparison = comparePrecedence(token, opStackTop);
-
-		/* 
-			Continously pop opStackTop into outStack only while:
-				1. opStack has elements.
-				2. opStackTop is not "(".
-				3. opStackTop has higher precedence compared to token OR opStackTop and token have equal precedence, and token is left associative.
-		*/
-		while (
-			opStack.length > 0 &&
-			opStackTop !== "(" &&
-			(precComparison === -1 ||
-				(precComparison === 0 &&
-					ASSOCIATION[token] === LEFT_ASSOCIATION))
-		) {
-			outStack.push(opStack.pop());
-
-			opStackTop = opStack[opStack.length - 1];
-			precComparison = comparePrecedence(token, opStackTop);
-		}
-
-		opStack.push(token);
 	}
-}
 
-/* Generate the final postfix by popping the contents of opStack into outStack */
-while (opStack.length > 0) {
-	outStack.push(opStack.pop());
-}
+	/* Generate the final postfix by popping the contents of opStack into outStack */
+	while (opStack.length > 0) {
+		outStack.push(opStack.pop());
+	}
 
-console.log(outStack);
+	return outStack; // final postfix
+};
+
+const main = () => {
+	const input = document.getElementById("exp").value;
+	const infix = parseInput(input);
+	const postfix = parseInfix(infix);
+
+	console.log("Infix: ", infix);
+	console.log("Postfix: ", postfix);
+};
+
+document.getElementById("run").addEventListener("click", main);
