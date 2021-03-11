05c5cc18447336daf859477547c43015146a79e4
ashmitkx <ashmitk0507@gmail.com>
2021-03-11 15:45:34 +0530

    Split js code into modules, and updated readme.
    Separated logic (backend) code from UI (frontend) related code.

---
+++ logic.js
@@ -0,0 +1,199 @@
+"use strict";
+
+/* 
+	This module's exported evalInput function:
+	1. Receives an expression string
+	2. Parses it into infix form
+	3. Converts it into postfix form. 
+	4. Evaluates the postfix.
+	5. Returns the result.
+*/
+
+/* 
+	Definitions;
+	1. Number = operand = opr
+	2. Operator = op
+	3. Parenthesis = par
+*/
+
+/*
+	Precedence table for operators.
+	Higher the number, greater the precedence.
+*/
+const PRECEDENCE = {
+	"^": 3,
+	"/": 2,
+	"*": 2,
+	"+": 1,
+	"-": 1
+};
+
+/* 
+	Association table for operators.
+	For explaination: https://en.wikipedia.org/wiki/Operator_associativity	
+*/
+const LEFT_ASSOCIATION = -1,
+	RIGHT_ASSOCIATION = 1;
+const ASSOCIATION = {
+	"^": RIGHT_ASSOCIATION,
+	"/": LEFT_ASSOCIATION,
+	"*": LEFT_ASSOCIATION,
+	"+": LEFT_ASSOCIATION,
+	"-": LEFT_ASSOCIATION
+};
+
+const OP_REGEX = /[\+\-\*\/\^]/,
+	PAR_REGEX = /[\(\)]/,
+	/* Captures absolute values of numbers. */
+	ABS_NUM_REGEX = /\d+\.?\d*/,
+	/* 
+		Captures actual values of numbers. 
+		The pattern before the | is to capture the first digit's minus sign, if its negative, since it wont have any operators before its minus sign.
+	*/
+	NUM_REGEX = new RegExp(
+		`^-?${ABS_NUM_REGEX.source}|(?<=${OP_REGEX.source}|${PAR_REGEX.source})-?${ABS_NUM_REGEX.source}`
+	);
+
+const tokenIsOpr = (token) => NUM_REGEX.test(token);
+const tokenIsOp = (token) => OP_REGEX.test(token);
+
+/* 
+	Compares the precedence of two operators op1 and op2.
+	Returns: 
+		1 if op1 has higher precedence
+		-1 if op2 has higher precedence
+		0 if op1 and op2 have same precedence
+*/
+const comparePrecedence = (token, opStackTop) => {
+	const comparison = PRECEDENCE[token] - PRECEDENCE[opStackTop];
+	return Math.sign(comparison);
+};
+
+/* Perform the operator (op) on operand 1 (opr1) and operand 2 (opr2). */
+const evaluate = (opr1, opr2, op) => {
+	switch (op) {
+		case "^":
+			return opr1 ** opr2;
+		case "/":
+			return opr1 / opr2;
+		case "*":
+			return opr1 * opr2;
+		case "+":
+			return opr1 + opr2;
+		case "-":
+			return opr1 - opr2;
+	}
+};
+
+/* Return an array of all operands, operators and parenthesis extracted from the input using a regex.*/
+const parseInput = (input) =>
+	input.match(
+		new RegExp(
+			`${NUM_REGEX.source}|${OP_REGEX.source}|${PAR_REGEX.source}`,
+			"g"
+		)
+	);
+
+/* Shunting Yard Algorithm. */
+const parseInfix = (infix) => {
+	let outStack = [],
+		opStack = [];
+
+	let i = 0;
+	while (i < infix.length) {
+		let token = infix[i];
+		i++;
+
+		if (tokenIsOpr(token)) {
+			/* Operands are converted from strings to numbers, and directly pushed into outStack. */
+			outStack.push(parseFloat(token));
+		} else if (token === "(") {
+			/* ")" is directly pushed into opStack. */
+			opStack.push(token);
+		} else if (token === ")") {
+			/* 
+				")" is not added to opStack.
+				Pop all operators from opStack into outStack, until a "(" is found.
+				"(" is to be discarded.
+			*/
+			let opStackTop = opStack.pop();
+			while (opStackTop !== "(") {
+				outStack.push(opStackTop);
+				opStackTop = opStack.pop();
+			}
+		} else if (tokenIsOp(token)) {
+			/* Compare the precedence of the operator (token) and opStackTop */
+			let opStackTop = opStack[opStack.length - 1],
+				precComparison = comparePrecedence(token, opStackTop);
+
+			/* 
+				Continously pop opStackTop into outStack only while:
+					1. opStack has elements.
+					2. opStackTop is not "(".
+					3. opStackTop has higher precedence compared to token OR opStackTop and token have equal precedence, and token is left associative.
+			*/
+			while (
+				opStack.length > 0 &&
+				opStackTop !== "(" &&
+				(precComparison === -1 ||
+					(precComparison === 0 &&
+						ASSOCIATION[token] === LEFT_ASSOCIATION))
+			) {
+				outStack.push(opStack.pop());
+
+				opStackTop = opStack[opStack.length - 1];
+				precComparison = comparePrecedence(token, opStackTop);
+			}
+
+			/* Finally push the token into opStack */
+			opStack.push(token);
+		}
+	}
+
+	/* Generate the final postfix by popping the contents of opStack into outStack */
+	while (opStack.length > 0) {
+		outStack.push(opStack.pop());
+	}
+
+	return outStack; // final postfix
+};
+
+const evalPostfix = (postfix) => {
+	let workingStack = [],
+		i = 0;
+	while (i < postfix.length) {
+		let token = postfix[i];
+		i++;
+
+		if (tokenIsOpr(token)) {
+			/* Push operands into workingStack. */
+			workingStack.push(token);
+		} else if (tokenIsOp(token)) {
+			/* 
+			When an operator is found, pop out 2 operands (opr2, opr1) from workingStack, and evalute the 2 operands against the operator. 
+			Push the result back into workingStack. 
+			*/
+			const opr2 = workingStack.pop(), // Operand 2 pops out first
+				opr1 = workingStack.pop();
+			const result = evaluate(opr1, opr2, token);
+			workingStack.push(result);
+		}
+	}
+
+	/* workingStack will finally have only 1 element, i.e. the final result. */
+	return workingStack[0];
+};
+
+const evalInput = (input) => {
+	const infix = parseInput(input);
+	const postfix = parseInfix(infix);
+	const result = evalPostfix(postfix);
+
+	console.log("Infix: ", infix);
+	console.log("Postfix: ", postfix);
+	console.log("Result: ", result);
+
+	return result;
+};
+
+export { evalInput };
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 # Simple Calculator
 
-A web-based calculator that can perform simple operations.
+A web calculator that can perform simple operations, and also evaluate brackets using postfix evaulation. \
+Infix to Postfix conversion implemented using the [Shunting-Yard algorithm](https://en.wikipedia.org/wiki/Shunting-yard_algorithm#The_algorithm_in_detail).
